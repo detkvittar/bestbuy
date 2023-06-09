502b1aaaf4e606b89facff67dcbf8c6d9c419e28
Emilia Malmqvist <emilia.malmqvist@learnet.se>
2023-06-09 15:18:25 +0200

    Improved readability by condensing products in a list. Also added new products.

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from products import Product
+import products
 from store import Store
 from menu import Menu
 
-
 def main():
-    # Create some products
-    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = Product("MacBook Air M2", price=1450, quantity=100)
-    pixel = Product("Google Pixel 7", price=500, quantity=250)
+    # setup initial stock of inventory
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250),
+                    products.NonStockedProduct("Windows License", price=125),
+                    products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+                    ]
 
-    # Create a store and add the products to it
-    store = Store()
-    store.add_product(bose)
-    store.add_product(mac)
-    store.add_product(pixel)
-
+    store = Store(product_list)
     # Start the menu, taking store as parameter
     menu = Menu(store)
     menu.start()
