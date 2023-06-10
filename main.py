ebb46b601426504af277384a5d84527a5c023840
Emilia Malmqvist <emilia.malmqvist@learnet.se>
2023-06-10 17:32:56 +0200

    Added promotions and styled according to PEP8

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import products
+import promotions
+
 from store import Store
 from menu import Menu
 
+
 def main():
-    # setup initial stock of inventory
-    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    products.Product("Google Pixel 7", price=500, quantity=250),
-                    products.NonStockedProduct("Windows License", price=125),
-                    products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
-                    ]
+    # set up inventory
+    product_list = [
+        products.Product("MacBook Air M2", price=1450, quantity=100),
+        products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+        products.Product("Google Pixel 7", price=500, quantity=250),
+        products.NonStockedProduct("Windows License", price=125),
+        products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+    ]
+
+    # Create promotions
+    second_half_price = promotions.SecondHalfPrice("Second Half price!")
+    third_one_free = promotions.ThirdOneFree("Third One Free!")
+    thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
+
+    # Add promotions to products
+    product_list[0].set_promotion(second_half_price)
+    product_list[1].set_promotion(third_one_free)
+    product_list[3].set_promotion(thirty_percent)
 
     store = Store(product_list)
-    # Start the menu, taking store as parameter
+
+    # Start menu, taking store as parameter
     menu = Menu(store)
     menu.start()
 
+
 if __name__ == "__main__":
-  main()
+    main()
